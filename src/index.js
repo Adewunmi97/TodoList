37646650ae54682b7fe29c79c3f82811a01b632e
Adewunmi97 <shonibareadewunmicomfort@gmail.com>
2021-09-23 10:03:41 +0100

    update index.html file and style.css

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import { handleDragStart, handleDrop, cancelDefault } from './services/sort';
 import {
-  updateStatus, getTodos,
+  updateStatus, getTodos, saveToStorage, orderAndSave,
 } from './services/update';
+import {
+  createTodo, clearAll, clearList, editTodoInStorage,
+  editTodoInList, deleteTodoFromStorage, deleteTodoFromList,
+} from './services/todoService';
 import './style.css';
 
 class TodoList {
   constructor() {
-    this.todos = [
-      { description: 'Item 1', completed: false, index: 1 },
-      { description: 'Item 2', completed: false, index: 2 },
-      { description: 'Item 3', completed: false, index: 3 },
-    ];
+    this.todos = getTodos();
   }
 
  handleCompletedStatus = (e) => {
@@ -18,9 +18,44 @@ class TodoList {
    updateStatus(e, todos);
  }
 
+ handleEditTodo = (e) => {
+   if (e.keyCode === 13) {
+     const { value } = e.target;
+     const index = parseInt(e.target.dataset.index, 10) - 1;
+     editTodoInStorage(value, index);
+     editTodoInList(value, index);
+   }
+ }
+
+ handleDeleteTodo = (e) => {
+   const index = parseInt(e.target.dataset.index, 10) - 1;
+   deleteTodoFromList(index);
+   deleteTodoFromStorage(index);
+ }
+
+ handleClearAllCompleted = () => {
+   const todos = getTodos();
+   const filteredTodos = clearAll(todos);
+   orderAndSave(filteredTodos);
+   clearList();
+   this.displayList();
+ }
+
+ handleNewTodo = (e) => {
+   const todos = getTodos();
+   if (e.keyCode === 13) {
+     const newTodo = createTodo(e.target.value, todos);
+     e.target.value = '';
+     todos.push(newTodo);
+     clearList();
+     saveToStorage(todos);
+     this.displayList();
+   }
+ }
+
  displayList() {
    const container = document.querySelector('.todoList');
-   this.todos.forEach((todo, index) => {
+   getTodos().forEach((todo, index) => {
      const li = document.createElement('li');
      li.innerHTML = `
                   <div class="task-item">
@@ -47,6 +82,20 @@ class TodoList {
      li.addEventListener('dragenter', cancelDefault);
      li.addEventListener('dragover', cancelDefault);
      container.appendChild(li);
+   });
+   document.querySelectorAll('input[type="checkbox"]').forEach((check) => {
+     check.addEventListener('change', this.handleCompletedStatus, false);
+   });
+   document.querySelector('#newTodo').addEventListener('keypress', this.handleNewTodo, false);
+
+   document.querySelector('#clear-all').addEventListener('click', this.handleClearAllCompleted, false);
+
+   document.querySelectorAll('input.edit').forEach((input) => {
+     input.addEventListener('keypress', this.handleEditTodo, false);
+   });
+
+   document.querySelectorAll('.todo-delete').forEach((trash) => {
+     trash.addEventListener('click', this.handleDeleteTodo, false);
    });
  }
 }
